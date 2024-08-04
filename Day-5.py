a1e4b8e7b951fce597b711842365360909c25070
ManashNepal <manash.nepal111@gmail.com>
2024-08-05 00:04:06 +0530

    my approach on random password generator

--- Day-5.py
+++ Day-5.py
@@ -1,16 +1,33 @@
-# Input a Python list of student heights
-student_heights = input().split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-total_height = 0
-student_count = 0
-for height in student_heights:
-  total_height += height
-  student_count += 1
-print(f"total height = {total_height}")
-print(sum(student_heights))
-print(f"number of students = {student_count}")
-average_height = round(total_height/student_count)
-print(f"average height = {average_height}")
-# Write your code below this row 👇
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+first_password = ''
+for n in range(0,nr_letters):
+    first_password += random.choice(letters)
+for n in range(0,nr_symbols):
+    first_password += random.choice(symbols)
+for n in range(0,nr_numbers):
+    first_password += random.choice(numbers)
+print(f"The password is: {first_password}")
+
+string_list = list(first_password)
+
+random.shuffle(string_list)
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+rand_password = ''.join(string_list)
+
+print(f"The random password is: {rand_password}")
