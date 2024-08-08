5a3e23ebedc38cbbf087c3e160969b35347739d1
ManashNepal <manash.nepal111@gmail.com>
2024-08-09 00:53:40 +0530

    my approach full game

--- Day-7.py
+++ Day-7.py
@@ -1,5 +1,63 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 word_list = ["aardvark","baboon","camel"]
 
 chosen_word = random.choice(word_list)
@@ -13,14 +71,24 @@ print(placeholder)
 
 listholder = list(placeholder)
 
+currentStage = 6
 
 while True:
-    if "_" in listholder:
+    if "_" in listholder and currentStage == 0:
+        print("You Lostt!!")
+        break
+    elif "_" in listholder:
         guess = input("Guess a letter: ").lower()
+        count = 0
         for n in range(len(chosen_word)):
             if chosen_word[n] == guess:
                 listholder[n] = guess
+                count += 1
+        if count == 0:
+            currentStage -= 1
         print("".join(listholder))
+        print(stages[currentStage])
     else:
+        print("You Wonn!")
         break
-print("You Wonn!")
+
