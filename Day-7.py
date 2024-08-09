665807a5433533c8eb6a21b335b219f230c20365
ManashNepal <manash.nepal111@gmail.com>
2024-08-09 09:52:52 +0530

    complete Day-7 project

--- Day-7.py
+++ Day-7.py
@@ -1,94 +1,41 @@
 import random
+from hangman_art import *
+from hangman_words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-
-word_list = ["aardvark","baboon","camel"]
+print(logo)
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
 
-placeholder = ""
+placeholder = []
 
 for n in range(len(chosen_word)):
-    placeholder += "_"
-print(placeholder)
-
-listholder = list(placeholder)
+    placeholder.append("_")
+print("".join(placeholder))
 
 currentStage = 6
 
 while True:
-    if "_" in listholder and currentStage == 0:
-        print("You Lostt!!")
+    if "_" in placeholder and currentStage == 0:
+        print("**********---You Lostt!!---**********")
+        print(f"The word was {chosen_word.upper()}")
         break
-    elif "_" in listholder:
+    elif "_" in placeholder:
         guess = input("Guess a letter: ").lower()
         count = 0
+        if guess in placeholder:
+            print(f"You have already guessed {guess} before and it is in the list.")
         for n in range(len(chosen_word)):
             if chosen_word[n] == guess:
-                listholder[n] = guess
+                placeholder[n] = guess
                 count += 1
         if count == 0:
             currentStage -= 1
-        print("".join(listholder))
+            print(f"Incorrect Guess!! You have {currentStage}/6 lives left.")
+        else:
+            print(f"Correct Guess!! You still have {currentStage}/6 lives left.")
+        print("".join(placeholder))
         print(stages[currentStage])
     else:
-        print("You Wonn!")
+        print("**********---You Wonn!!---**********")
         break
 
