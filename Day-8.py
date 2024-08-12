4c7ad9fe34c89941fe62e460d7c41b91276d6aea
ManashNepal <manash.nepal111@gmail.com>
2024-08-12 23:51:48 +0530

    my approach to cipher text - 1

--- Day-8.py
+++ Day-8.py
@@ -1,8 +1,17 @@
-def life_in_weeks(age):
-    remaining_years = 90-age
-    remaining_weeks = remaining_years * 52
-    print(f"You have {remaining_weeks} left.")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-age = int(input())
-life_in_weeks(age)
+def encrypt(original_text,shift_amount):
+    text_list = list(original_text)
+    for letter in text_list:
+        alphabet_index = alphabet.index(letter) 
+        new_char = alphabet[(alphabet_index+shift_amount)%26]
+        list_index = text_list.index(letter)
+        text_list[list_index] = new_char
+    print("".join(text_list))
+
+encrypt(text,shift)
---
+++ Practice.py
@@ -0,0 +1,6 @@
+name = "Manash"
+
+name_list = list(name)
+print(name_list)
+
+print(name_list.index("a"))
