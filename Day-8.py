ff3a01ddc6cc1c3905e61fae7430dd42137ce7da
ManashNepal <manash.nepal111@gmail.com>
2024-08-14 00:57:12 +0530

    finish Day-8 project

--- Day-8.py
+++ Day-8.py
@@ -1,42 +1,29 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from caesar_art import logo
+print(logo)
 
-
-def ceaser(direction,original_text,shift_amount):
+def caeser(direction,original_text,shift_amount):
     result = " "
     shifted_position = 0
     for letter in original_text:
-        if letter == " ":
-            pass
+        if letter not in alphabet:
+            result += letter
         else:
             if direction == "encode":
                 shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet) 
             else:
               shifted_position = (alphabet.index(letter) - shift_amount)  
             result += alphabet[shifted_position]
-    print(f"Here is the result: {result}")
+    print(f"Here is the {direction}d code: {result}")
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+flag = True
 
-    # if direction == "encode":
-    #     encoded_code = ""
-    #     for letter in original_text:
-    #         if letter == " ":
-    #             pass
-    #         else:
-    #             shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet) 
-    #             encoded_code += alphabet[shifted_position]
-    #     print(f"Here is the encoded result: {encoded_code}")
-    # else:
-    #     decoded_code = ""
-    #     for letter in original_text:
-    #         if letter == " ":
-    #             pass
-    #         else:
-    #             shifted_pos = alphabet.index(letter) - shift_amount
-    #             decoded_code += alphabet[shifted_pos]
-    #     print(f"Here is the decoded result: {decoded_code}")
-
-
-ceaser(direction=direction,original_text=text,shift_amount=shift)
+while flag:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(direction=direction,original_text=text,shift_amount=shift)
+    decision = input("Do you want to continue? (Y/N):\n").lower()
+    if decision == "n":
+        flag = False
+        print("Thank you for using our program!!")
