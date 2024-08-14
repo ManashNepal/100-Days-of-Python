b4912cb1b7e6284baf7db3022cf9c8dcdfd90a67
ManashNepal <manash.nepal111@gmail.com>
2024-08-14 21:36:28 +0530

    finish project

--- Day-9.py
+++ Day-9.py
@@ -1,24 +1,31 @@
+import os
 from secret_auction_art import *
+
+def get_highest_bidder(bid_dictionary):
+    highest_bid = 0
+    highest_bidder = ""
+
+    for bidder in bid_dictionary:
+        if bid_dictionary[bidder] > highest_bid:
+            highest_bid = bid_dictionary[bidder]
+            highest_bidder = bidder
+
+    print(f"The winner is {highest_bidder} with a bid of Rs.{highest_bid}.")
 
 print(logo)
 
-bidder_info = {"Dummy":0}
+bid_dictionary = {}
 flag = True
 
-while flag:
-    name = input("Enter the bidder name: ")
-    bid = int(input("Enter your bid: $"))
-    bidder_info[name] = bid
+while flag: 
+    name = input("Enter your name: ")
+    bid = int(input("Enter your bid: Rs."))
+    bid_dictionary[name] = bid
     decision = input("Are there other bidders(yes/no)?: ").lower()
 
     if decision == "no":
         flag = False
+    
+    os.system('cls')
 
-
-max_bidder = "Dummy"
-
-for key in bidder_info:
-    if bidder_info[key] > bidder_info[max_bidder]:
-        max_bidder = key
-
-print(f"The winner is {max_bidder} with a bid of ${bidder_info[max_bidder]}.")
+get_highest_bidder(bid_dictionary)
