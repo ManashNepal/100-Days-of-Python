3dac14425f0db13229ce45c5eca1cd78ae28cfa6
ManashNepal <manash.nepal111@gmail.com>
2024-09-11 08:14:49 +0530

    complete

--- Day_12.py
+++ Day_12.py
@@ -1,8 +1,8 @@
 import random
 
-difficulty = input("Choose a difficulty between 'easy' and 'hard': ")
-unknown_number = random.randint(1,100)
-
+#It is a way to create global constant like for PI = 3.14
+EASY_LEVEL_ATTEMPTS = 10
+HARD_LEVEL_ATTEMPTS = 5
 
 def is_guess_correct(num):
     guess_num = int(input("Make a guess: "))
@@ -14,17 +14,21 @@ def is_guess_correct(num):
         if guess_num > num:
             return 1
 
+print("Welcome to the Number Guessing Game!!")
+print("I'm thinking of a number between between 1 and 100.")
+unknown_number = random.randint(1,100)
+difficulty = input("Choose a difficulty between 'easy' and 'hard': ")
+
 attempts = 0
 if difficulty == "easy":
-    attempts = 10
+    attempts = EASY_LEVEL_ATTEMPTS
 else:
-    attempts = 5    
-
-print(f"You have {attempts} attempts remaining to guess the number.")
+    attempts = HARD_LEVEL_ATTEMPTS    
     
-while attempts != 0:
+while True:
+    print(f"You have {attempts} attempts remaining to guess the number.")
     guess = is_guess_correct(unknown_number)
-    attempts -= 1
+    attempts -= 1   
     if guess == 0 :
         print(f"You got it! The answer was {unknown_number}.")
         break
@@ -36,5 +40,5 @@ while attempts != 0:
         print(f"You've run out of guesses, you loose.")
         break
     print("Guess again.")
-    print(f"You have {attempts} attempts remaining to guess the number.")
+    
 
