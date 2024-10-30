82e78348b570f89c11516b49f088d193c3231338
ManashNepal <manash.nepal111@gmail.com>
2024-10-30 17:59:26 +0530

    minor change

--- Day_24/main.py
+++ Day_24/main.py
@@ -1,12 +1,12 @@
-with open("Day_24/Input/Names/invited_names.txt") as name_file:
+with open("./Input/Names/invited_names.txt") as name_file:
     names = name_file.readlines()
 
-with open("Day_24/Input/Letters/starting_letter.txt") as letter_template:
+with open("./Input/Letters/starting_letter.txt") as letter_template:
     contents = letter_template.read()
 
 
 for name in names:
     receiver = name.strip()
     new_contents = contents.replace("[name]",receiver)
-    with open(f"Day_24/Output/ReadyToSend/{receiver}.txt", mode="w") as ready_letters:
+    with open(f"./Output/ReadyToSend/letter_for_{receiver}.txt", mode="w") as ready_letters:
         letters = ready_letters.write(new_contents)
